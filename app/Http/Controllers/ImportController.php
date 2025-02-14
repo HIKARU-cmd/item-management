9a06b78cd130e3b698c17d38ae0d4c7e71d044c6
HIKARU-cmd <hikaru.hemmi@outlook.jp>
2025-02-14 23:59:04 +0900

    バリデーションエラー時の行数表示機能、一括保存へ変更

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -20,23 +20,32 @@ class ImportController extends Controller
         // csvファイルを取得
         $file = $request->file('csvFile');
         
-        // ファイルの内容確認
+        // ファイルの内容確認 "r"は読み込み専用
         $handle = fopen($file, "r");
         if(!$handle){
             return redirect('/items/add')->with('error', 'ファイルを開けません。');
         }
         
-        // ヘッダー行を読み込み
+        // ヘッダー行を配列で格納
         $header = fgetcsv($handle);
         $header[0] = str_replace("\u{FEFF}", '', $header[0]);
-        
+        $lineNumber = 0;
+
         // csvデータの処理
         while(($row = fgetcsv($handle)) !== false ){
-            // csvファイルのカラムとデータを連想配列として格納
-            $data = array_combine($header, $row);
+
+            // 空の行はカウント無し
+            if(empty(array_filter($row))){
+                continue;
+            };
+
+            // 行番号をインクリメント
+            $lineNumber++;
+            // csvファイルのカラムとデータを連想配列として生成、$headerをkey、$rowをvalueとして配列を生成
+            $rowData = array_combine($header, $row);
             
             // csvファイルデータのバリデーション
-            $validator = Validator::make($data, [
+            $validator = Validator::make($rowData, [
                 'name' => 'required|max:100',
                 'process_id' => 'required|exists:processes,id',
                 'price' => 'required|integer|min:0',
@@ -48,23 +57,26 @@ class ImportController extends Controller
             // バリデーション失敗時のエラー表示
             if ($validator->fails()) {
                 fclose($handle);
-                return redirect('/items')->withErrors($validator);
+                return redirect('/items')->withErrors($validator)->with('error', $lineNumber. '行目の入力形式を確認してください。インポートできません。');
             }
 
-            // データをデータベースに保存
-            Item::create([
+            $data[] = [
                 'user_id' => Auth::user()->id,
-                'name' => $data['name'],
-                'process_id' => $data['process_id'],
-                'price' => $data['price'],
-                'quantity' => $data['quantity'],
-                'purchase_at' => $data['purchase_at'],
-                'detail' => $data['detail'] ?? null,
-            ]);
-        }
+                'name' => $rowData['name'],
+                'process_id' => $rowData['process_id'],
+                'price' => $rowData['price'],
+                'quantity' => $rowData['quantity'],
+                'purchase_at' => $rowData['purchase_at'],
+                'detail' => $rowData['detail'] ?? null,
+            ];
 
+        }
+                
         // ファイルを閉じる
         fclose($handle);
+
+            // データをデータベースに保存
+            Item::insert($data);
 
         return redirect('/items')->with('success', 'csvファイルを正常にインポートしました。');
 
